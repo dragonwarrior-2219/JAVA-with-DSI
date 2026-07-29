a80f4c53f5d8eb10efa1e0bf2721b603efda5f86
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-07-29 09:18:09 +0530

    Add Java program for addition of two numbers
    Implement a simple Java program to add two user-input numbers.

---
+++ addition of two numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Number: ");
+        int x = scanner.nextInt();
+        System.out.println("Enter Second Number: ");
+        int y = scanner.nextInt();
+        System.out.print(x+y);
+        }
+}
+  
