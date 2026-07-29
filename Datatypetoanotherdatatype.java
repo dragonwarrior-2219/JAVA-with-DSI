b43035f5a1a573af5531aa257c11560b8fb7d603
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-07-29 11:56:57 +0530

    Add data type conversion examples in Java

---
+++ Datatypetoanotherdatatype.java
@@ -0,0 +1,68 @@
+import java.util.*;
+class Intdouble {
+    public static void main(String[] args) {
+        //INT TO DOUBLE
+        int x = 24;
+        int y = 56;
+        
+        double a = x;
+        double b = y;
+        
+        System.out.println(a);
+        System.out.println(b);
+        // DOUBLE TO INT
+        double z = 24.0;
+        double i = 56.0;
+        
+        
+        int c = (int)z;
+        int d = (int)i;
+        
+        System.out.println(c);
+        System.out.println(d);
+        //INT TO FLOAT
+        int j = 24;
+        int k = 56;
+        
+        
+        float e = (float)j;
+        float f = (float)k;
+        
+        System.out.println(e);
+        System.out.println(f);
+        
+        //FLOAT TO INT
+        float l = 24.0f;
+        float m = 56.0f;
+        
+        
+        int g = (int)l;
+        int h = (int)m;
+        
+        System.out.println(g);
+        System.out.println(h);
+        
+        //DOUBLE TO FLOAT
+        double n = 24.0;
+        double o = 56.0;
+        
+        
+        float A = (float)n;
+        float B = (float)o;
+        
+        System.out.println(A);
+        System.out.println(B);
+        
+        //FLOAT TO DOUBLE
+        float p = 24.0f;
+        float q = 56.0f;
+        
+        
+        double C = (double)p;
+        double D = (double)q;
+        
+        System.out.println(C);
+        System.out.println(D);
+        }
+}
+        
