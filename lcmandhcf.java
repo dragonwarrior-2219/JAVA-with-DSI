796a0a03724e6565d894abe5710e19138a93aafe
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-07-29 14:52:35 +0530

    Refactor LCM and HCF calculation to use user inputs

--- lcmandhcf.java
+++ lcmandhcf.java
@@ -1,12 +1,18 @@
 import java.util.*;
-class Main {
+public class Main {
+
+    static int hcf(int a, int b) {
+        if (b == 0)
+            return a;
+        return hcf(b, a % b);
+}
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter First Number: ");
-        int x = scan.nextInt();
+        int a = scan.nextInt();
         System.out.println("Enter Second Number: ");
-        int y = scan.nextInt();
-        int hcf = hcf(x,y);
+        int b = scan.nextInt();
+        int hcf = hcf(a,b);
         int lcm = ((a*b)/hcf);
         System.out.println("LCM is" + lcm);
         System.out.println("HCF is" + hcf);
