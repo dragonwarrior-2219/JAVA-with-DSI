bd7351c134deff5b9808798ea303853fc226bb82
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-07-29 09:06:24 +0530

    Deployed the correct code!

--- Leapyear.java
+++ Leapyear.java
@@ -1,14 +1,11 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a year: ");
-        int x = scan.nextInt();
-        int y = x%4==0 && x%100!=0 || x %400 == 0;
-        y ? "Leap Year" : "Not a leap year";
-        System.out.print(y);
+        int x = scanner.nextInt();
+        boolean y = x%4==0 && x%100!=0 || x %400 == 0;
+        System.out.print(y? "Leap Year" : "Not a leap year");
         }
 }
-        
+   
