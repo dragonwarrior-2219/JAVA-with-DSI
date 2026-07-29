2cdfa8324c99159a74fb78f75346387f7463a62a
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-07-29 11:26:00 +0530

    Refactor random number generation in Oddoreven.java

--- Oddoreven.java
+++ Oddoreven.java
@@ -1,7 +1,8 @@
 import java.util.*;
 class Main {
     public static void main(String[] args) {
-        int x = (int) (Math.random()*(100) + 1);
+        Random ran = new Random();
+        int x = ran.nextInt(100) + 1;
         System.out.println(x);
         boolean y = (x%2==0);
         System.out.println(y?"It is a Even Number" : "It is a Odd Number");
