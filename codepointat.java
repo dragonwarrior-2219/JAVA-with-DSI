df3530686c898479103c38aca778a094dfab984a
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-03 15:51:09 +0530

    Add codepointat.java to demonstrate codePointAt usage

---
+++ codepointat.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        String s = "ABC";
+        int code = s.codePointAt(0);
+        System.out.println("Code Point = " + code);
+    }
+}
