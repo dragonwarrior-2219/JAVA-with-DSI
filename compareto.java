db5195818fb55c67f13c2cc7d15ba32e84c2f173
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-03 15:55:15 +0530

    Add compareTo example in compareto.java

---
+++ compareto.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        String s1 = "Apple";
+        String s2 = "Banana";
+        int result = s1.compareTo(s2);
+        System.out.println("Result = " + result);
+    }
+}
